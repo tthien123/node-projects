4833a7eb1a070870a4b101ab74bfbaf2534e7b29
tth <tran.thai.hien@talentnet.vn>
2020-06-13 16:51:04 +0700

    ewrew

--- controller/index.js
+++ controller/index.js
@@ -1,13 +1,13 @@
 const productJson = require('../json.datas/products.json')
 
-module.exports.getProducts = async (request, response, next) => {
+module.exports.getProducts = (request, response, next) => {
  console.log(productJson.body);
   response.render('products', {
     products: productJson.body
   })
 }
 
-module.exports.getProductsDetail = async (request, response, next) => {
+module.exports.getProductsDetail = (request, response, next) => {
   const productdetail = productJson.body.find((p) => p.id === request.params.id)
 
   response.render('products', {
